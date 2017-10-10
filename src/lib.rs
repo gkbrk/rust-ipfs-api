6358aba8bab9bf85174304caa841bc1d3326d4cb
Gökberk Yaltıraklı <webdosusb@gmail.com>
2017-10-10 14:19:59 +0100

    Splitted lib.rs into modular pieces

--- src/lib.rs
+++ src/lib.rs
@@ -11,25 +11,12 @@ extern crate serde;
 extern crate base64;
 
 use std::io::Read;
-use std::io::{BufReader, BufRead};
+
+mod pubsub;
 
 pub struct IpfsApi {
     server: String,
     port: u16
-}
-
-#[derive(Deserialize)]
-struct JsonPubSubMessage {
-    data: String,
-    from: String,
-    seqno: String
-}
-
-#[derive(Debug)]
-pub struct PubSubMessage {
-    pub data: Option<Vec<u8>>,
-    pub from: Option<Vec<u8>>,
-    pub seqno: Option<Vec<u8>>
 }
 
 /// The main interface of the library
@@ -63,47 +50,5 @@ impl IpfsApi {
         let url = format!("http://{}:{}/api/v0/cat?arg={}", self.server, self.port, hash);
         let resp = reqwest::get(&url)?;
         Ok(resp.bytes().filter(|x|x.is_ok()).map(|x|x.unwrap()))
-    }
-
-    /// Subscribes to a channel for p2p messages
-    /// This function subscribes to a channel/topic and listens to any data
-    /// sent to it. It can be used for peer-to-peer communication and dynamic
-    /// apps over IPFS.
-    ///
-    /// ```
-    /// let api = IpfsApi::new("127.0.0.1", 5001);
-    ///
-    /// for message in api.pubsub_subscribe("chat")? {
-    ///     println!("{:?}", message);
-    /// }
-    /// ```
-    pub fn pubsub_subscribe(&self, channel: &str) -> Result<impl Iterator<Item=PubSubMessage>, reqwest::Error> {
-        let url = format!("http://{}:{}/api/v0/pubsub/sub?arg={}&discover=true", self.server, self.port, channel);
-        let resp = reqwest::get(&url)?;
-
-        let messages = BufReader::new(resp).lines()
-            .filter(|x|x.is_ok())
-            .map(|x|x.unwrap())
-            .map(|x|serde_json::from_str::<JsonPubSubMessage>(&x))
-            .filter(|x|x.is_ok())
-            .map(|x|x.unwrap())
-            .map(|x| {
-                PubSubMessage {
-                    from: base64::decode(&x.from).ok(),
-                    seqno: base64::decode(&x.seqno).ok(),
-                    data: base64::decode(&x.data).ok()
-                }
-            });
-
-        Ok(messages)
-    }
-
-    /// Sends a p2p message to a channel
-    /// This function sends a data packet to a channel/topic. It can be used
-    /// for peer-to-peer communication and dynamic apps over IPFS.
-    pub fn pubsub_publish(&self, channel: &str, data: &str) -> Result<(), reqwest::Error> {
-        let url = format!("http://{}:{}/api/v0/pubsub/pub?arg={}&arg={}", self.server, self.port, channel, data);
-        let resp = reqwest::get(&url)?;
-        Ok(())
     }
 }
---
+++ src/pubsub.rs
@@ -0,0 +1,78 @@
+use std::io::{BufReader, BufRead};
+
+use IpfsApi;
+
+extern crate reqwest;
+extern crate serde_json;
+extern crate base64;
+
+#[derive(Deserialize)]
+struct JsonPubSubMessage {
+    data: String,
+    from: String,
+    seqno: String
+}
+
+pub struct PubSubMessage {
+    data: Option<Vec<u8>>,
+    from: Option<Vec<u8>>,
+    seqno: Option<Vec<u8>>
+}
+
+impl PubSubMessage {
+    pub fn data(&self) -> Option<Vec<u8>> {
+        self.data.clone()
+    }
+
+    pub fn from(&self) -> Option<Vec<u8>> {
+        self.from.clone()
+    }
+
+    pub fn seqno(&self) -> Option<Vec<u8>> {
+        self.seqno.clone()
+    }
+}
+
+impl IpfsApi {
+    /// Subscribes to a channel for p2p messages
+    /// This function subscribes to a channel/topic and listens to any data
+    /// sent to it. It can be used for peer-to-peer communication and dynamic
+    /// apps over IPFS.
+    ///
+    /// ```
+    /// let api = IpfsApi::new("127.0.0.1", 5001);
+    ///
+    /// for message in api.pubsub_subscribe("chat")? {
+    ///     println!("{:?}", message);
+    /// }
+    /// ```
+    pub fn pubsub_subscribe(&self, channel: &str) -> Result<impl Iterator<Item=PubSubMessage>, reqwest::Error> {
+        let url = format!("http://{}:{}/api/v0/pubsub/sub?arg={}&discover=true", self.server, self.port, channel);
+        let resp = reqwest::get(&url)?;
+
+        let messages = BufReader::new(resp).lines()
+            .filter(|x|x.is_ok())
+            .map(|x|x.unwrap())
+            .map(|x|serde_json::from_str::<JsonPubSubMessage>(&x))
+            .filter(|x|x.is_ok())
+            .map(|x|x.unwrap())
+            .map(|x| {
+                PubSubMessage {
+                    from: base64::decode(&x.from).ok(),
+                    seqno: base64::decode(&x.seqno).ok(),
+                    data: base64::decode(&x.data).ok()
+                }
+            });
+
+        Ok(messages)
+    }
+    
+    /// Sends a p2p message to a channel
+    /// This function sends a data packet to a channel/topic. It can be used
+    /// for peer-to-peer communication and dynamic apps over IPFS.
+    pub fn pubsub_publish(&self, channel: &str, data: &str) -> Result<(), reqwest::Error> {
+        let url = format!("http://{}:{}/api/v0/pubsub/pub?arg={}&arg={}", self.server, self.port, channel, data);
+        let _resp = reqwest::get(&url)?;
+        Ok(())
+    }
+}
