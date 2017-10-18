e6395a355409b689e4830d7451104a7984ef5297
Gökberk Yaltıraklı <webdosusb@gmail.com>
2017-10-18 16:05:48 +0100

    Added accessor methods for the object API

--- src/object.rs
+++ src/object.rs
@@ -14,7 +14,35 @@ error_chain! {
 #[serde(rename_all="PascalCase")]
 pub struct ObjectStats {
     hash: String,
+    num_links: u64,
+    block_size: u64,
+    links_size: u64,
+    data_size: u64,
     cumulative_size: u64
+}
+
+impl ObjectStats {
+    pub fn hash(&self) -> String {
+        self.hash.clone()
+    }
+
+    pub fn num_links(&self) -> u64 {
+        self.num_links
+    }
+
+    pub fn block_size(&self) -> u64 {
+        self.block_size
+    }
+
+    pub fn links_size(&self) -> u64 {
+        self.links_size
+    }
+
+    /// Get the total size an IPFS hash takes. Includes all the recursive
+    /// directories and files.
+    pub fn cumulative_size(&self) -> u64 {
+        self.cumulative_size
+    }
 }
 
 impl IpfsApi {
